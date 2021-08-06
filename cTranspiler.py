0b06c3236a50413a0aa5e363840772e7671bb5e2
eualexdev <eujosealexsandro@gmail.com>
2021-08-06 12:11:44 -0300

    Removi alguns bug

--- cTranspiler.py
+++ cTranspiler.py
@@ -12,7 +12,7 @@ def debug(a="",b=""):pass
 import os,sys,shutil
 from re import T
 import libs
-from variables import GetFinalExtension, extForBuild,GetPlatform
+from variables import GetFinalExtension, extForBuild
 from langLexer import Lexer, TT_COMENT, TT_DEFINATION, TT_KEYWORD, TT_SYMBOL, TT_TYPE, types,keywords
 from langParse import Parser
 from msg import generated_code_for_insert
@@ -185,12 +185,11 @@ class Transpiler:
         with open(self.fileBuild,"wt+") as file:
             file.write(self.lang)
         
+        os.system("g++ "+self.fileBuild +" -w -O3 -o "+self.fileBuild_Name)
         fileNormalBuild = os.path.join(self.languagePathNormal,fileBuilded)
-        os.system("g++ "+self.fileBuild +" -w -O3 -o "+fileNormalBuild)
-        # if os.path.exists(fileNormalBuild):
-        #     os.remove(fileNormalBuild)
-        # os.system(f"{mv_move} {self.fileBuild_Name} {self.languagePathNormal}")
-        print(fileNormalBuild)
+        if os.path.exists(fileNormalBuild):
+            os.remove(fileNormalBuild)
+        shutil.move(self.fileBuild_Name,self.languagePathNormal)
         os.system(f"{fileNormalBuild} {' '.join(sys.argv[2:])}")
         # print(self.fileBuild,self.fileBuild_Name)
         # print(fileBuilded)
